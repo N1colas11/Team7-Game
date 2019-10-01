d5dd0544aca2d67f1c77d1bab0940b72b2a25149
N1colas11 <fusrodah81@gmail.com>
2019-09-30 20:40:54 -0400

    First major step in game development. Added a Main menu, an options menu, a pause menu, and a game interface. So far there is a prefab of a bear that is the player for the time being until we create a proper sprite. The other animations and sprites in the Game scene are just for testing purposes.  They will also change once we create the sprites for them. Player can move back and forth, and can jump and punch. Background is a never ending scroller which was made with a script. There is a default title theme music that plays, as well as music for when in game.
    Need to implement:
    Sound effects
    A proper main menu background
    Scenes are breaking when switching between them (Ex when in main menu < play < Pause button does not work... BUT if game is started directly to play, the pause button DOES work... need to troubleshoot
    Proper character sprites
    Many more things

---
+++ Assets/SettingsMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer Audiomixer;
+    public Slider slider;
+
+    private void Start()
+    {
+        slider.onValueChanged.AddListener(SetVolume);
+    }
+    public void SetVolume(float targetVolume)
+    {
+        Audiomixer.SetFloat("volume", targetVolume);
+    }
+
+    /*public AudioMixer audioMixer;
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }*/
+}
